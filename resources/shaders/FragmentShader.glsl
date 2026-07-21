993520a13f2597b435e105b8dc01a311d43a65cc
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-21 22:32:50 +0200

    adershit betterligthy

--- resources/shaders/FragmentShader.glsl
+++ resources/shaders/FragmentShader.glsl
@@ -6,8 +6,19 @@ in vec3 FragPos;
 in vec2 TexCoord;
 
 uniform sampler2D ourTexture;
+uniform vec3 objectColor;
 
 void main()
 {
-    FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 texColor = texture(ourTexture, TexCoord);
+    vec4 baseColor = vec4(texColor.rgb * objectColor, texColor.a);
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(vec3(0.0, 1.0, 0.0));
+    float diff = max(dot(norm, lightDir), 0.0);
+
+    vec3 ambient = 0.15 * baseColor.rgb;
+    vec3 diffuse = diff * baseColor.rgb;
+
+    FragColor = vec4(ambient + diffuse, baseColor.a);
 }
--- resources/shaders/FragmentShader_vk.glsl
+++ resources/shaders/FragmentShader_vk.glsl
@@ -2,6 +2,8 @@
 
 layout (location = 0) out vec4 FragColor;
 
+layout (location = 0) in vec3 Normal;
+layout (location = 1) in vec3 FragPos;
 layout (location = 2) in vec2 TexCoord;
 layout (location = 3) in vec4 ObjectColor;
 
@@ -14,5 +16,15 @@ layout(binding = 1) uniform sampler2D ourTexture;
 
 void main()
 {
-    FragColor = ObjectColor;
+    vec4 texColor = texture(ourTexture, TexCoord);
+    vec4 baseColor = texColor * ObjectColor;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(vec3(0.0, 1.0, 0.0));
+    float diff = max(dot(norm, lightDir), 0.0);
+
+    vec3 ambient = 0.15 * baseColor.rgb;
+    vec3 diffuse = diff * baseColor.rgb;
+
+    FragColor = vec4(ambient + diffuse, baseColor.a);
 }
